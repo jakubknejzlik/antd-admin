158c9b789636991830980f125c7e061080063876
Jakub Knejzlik <jakub.knejzlik@gmail.com>
2024-11-17 15:25:42 +0100

    Add user auth gateway

--- src/ui/layout/Layout.tsx
+++ src/ui/layout/Layout.tsx
@@ -1,16 +1,16 @@
-// import { useAuthenticator } from "@aws-amplify/ui-react";
 import { Dropdown, Flex, Layout, Space, theme } from "antd";
 
 import { DownOutlined, LogoutOutlined } from "@ant-design/icons";
 import { ItemType, MenuItemType } from "antd/es/menu/interface";
 import { PropsWithChildren } from "react";
 import { AdminLayoutMenu } from "./LayoutMenu";
+import { UserAuthGateway, UserAuthGatewayProps } from "./UserAuthGateway";
 
 const { Header, Content, Sider } = Layout;
 
 export interface AdminLayoutProps {
   menuItems?: ItemType<MenuItemType>[];
-  user?: { username: string };
+  user: UserAuthGatewayProps["user"];
   header?: React.ReactNode;
   signOut?: () => void;
   logo?: React.ReactNode;
@@ -29,61 +29,69 @@ export const AdminLayout = ({
   } = theme.useToken();
 
   return (
-    <Layout style={{ minHeight: "100vh" }}>
-      <Header style={{ padding: 0, paddingRight: "1.5rem" }}>
-        <Flex
-          justify="space-between"
-          align="center"
-          style={{
-            height: 64,
-          }}
-        >
-          {logo || (
-            <div style={{ width: "13rem", padding: "2.5rem", color: "white" }}>
-              <img src="/logo.png" alt="logo" />
-            </div>
-          )}
-          <Space>
-            {header}
-            <Dropdown
-              menu={{
-                items: [
-                  {
-                    key: "logout",
-                    label: "Logout",
-                    icon: <LogoutOutlined />,
-                    onClick: signOut,
-                  },
-                ],
+    <UserAuthGateway
+      user={user}
+      signOut={signOut}
+      render={({ user }) => (
+        <Layout style={{ minHeight: "100vh" }}>
+          <Header style={{ padding: 0, paddingRight: "1.5rem" }}>
+            <Flex
+              justify="space-between"
+              align="center"
+              style={{
+                height: 64,
               }}
             >
-              <a onClick={(e) => e.preventDefault()}>
-                <Space style={{ color: "white" }}>
-                  {user?.username}
-                  <DownOutlined />
-                </Space>
-              </a>
-            </Dropdown>
-          </Space>
-        </Flex>
-      </Header>
-      <Layout>
-        <Sider
-          collapsible
-          width={"13rem"}
-          style={{ background: colorBgContainer }}
-          className="w-52"
-        >
-          <AdminLayoutMenu items={menuItems} />
-        </Sider>
-        <Content
-          style={{
-            minHeight: 280,
-          }}
-        >
-          {children}
-        </Content>
-      </Layout>
-    </Layout>
+              {logo || (
+                <div
+                  style={{ width: "13rem", padding: "2.5rem", color: "white" }}
+                >
+                  <img src="/logo.png" alt="logo" />
+                </div>
+              )}
+              <Space>
+                {header}
+                <Dropdown
+                  menu={{
+                    items: [
+                      {
+                        key: "logout",
+                        label: "Logout",
+                        icon: <LogoutOutlined />,
+                        onClick: signOut,
+                      },
+                    ],
+                  }}
+                >
+                  <a onClick={(e) => e.preventDefault()}>
+                    <Space style={{ color: "white" }}>
+                      {user.username}
+                      <DownOutlined />
+                    </Space>
+                  </a>
+                </Dropdown>
+              </Space>
+            </Flex>
+          </Header>
+          <Layout>
+            <Sider
+              collapsible
+              width={"13rem"}
+              style={{ background: colorBgContainer }}
+              className="w-52"
+            >
+              <AdminLayoutMenu items={menuItems} />
+            </Sider>
+            <Content
+              style={{
+                minHeight: 280,
+              }}
+            >
+              {children}
+            </Content>
+          </Layout>
+        </Layout>
+      )}
+    />
   );
 };
---
+++ src/ui/layout/UserAuthGateway.tsx
@@ -0,0 +1,63 @@
+// import { useAuthenticator } from "@aws-amplify/ui-react";
+import { Button, Result, Spin } from "antd";
+
+import { useQuery } from "@tanstack/react-query";
+import { PropsWithChildren } from "react";
+
+type User = {
+  username: string;
+};
+type UserValue = User | null;
+
+export interface UserAuthGatewayProps {
+  user?: (() => Promise<UserValue>) | Promise<UserValue> | UserValue;
+  signOut?: () => void;
+  render: (props: { user: User }) => React.ReactNode;
+}
+
+export const UserAuthGateway = ({
+  render,
+  user,
+  signOut,
+}: PropsWithChildren<UserAuthGatewayProps>) => {
+  const userData = useQuery({
+    queryKey: ["user-auth-gateway", "user"],
+    queryFn: () => {
+      if (typeof user === "function") {
+        return user();
+      }
+      return user ?? null;
+    },
+  });
+
+  if (userData.isLoading) {
+    return <Spin style={{ width: "100%", height: "100%" }} />;
+  }
+
+  if (userData.error) {
+    return (
+      <Result
+        status="403"
+        title="Unauthorized"
+        subTitle={userData.error.message}
+      />
+    );
+  }
+
+  if (!userData.data) {
+    return (
+      <Result
+        status="403"
+        title="Unauthorized"
+        subTitle="Sorry, you are not authorized to access this page."
+        extra={
+          <Button type="primary" onClick={signOut}>
+            Logout
+          </Button>
+        }
+      />
+    );
+  }
+
+  return render({ user: userData.data });
+};
