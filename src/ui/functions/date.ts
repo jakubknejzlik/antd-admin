0096ea0852ea03fd9a35e030ecdb1acf86d8cc1e
Jakub Knejzlik <jakub.knejzlik@gmail.com>
2025-02-16 00:44:07 +0100

    Add formatDateTime function

--- src/ui/functions/date.ts
+++ src/ui/functions/date.ts
@@ -4,8 +4,22 @@ import localizedFormat from "dayjs/plugin/localizedFormat";
 dayjs.extend(localizedFormat);
 
 export function formatDate(
-  date: Dayjs | string | null,
+  date: Dayjs | Date | string | null,
   format: string = "l"
+): string {
+  if (!date) {
+    return "–";
+  }
+  const d = dayjs(date);
+  if (!d.isValid()) {
+    return "–";
+  }
+  return d.format(format);
+}
+
+export function formatDateTime(
+  date: Dayjs | Date | string | null,
+  format: string = "lll"
 ): string {
   if (!date) {
     return "–";
